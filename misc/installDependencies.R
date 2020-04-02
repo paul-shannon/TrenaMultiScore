4d8227e078e3938af85ae5e82f6a14c5555d922a
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-04-02 13:11:32 -0700

    to simplify new installation

---
+++ misc/installDependencies.R
@@ -0,0 +1,20 @@
+biocGet <- function(pkgs){
+   library(BiocManager)
+   BiocManager::install(pkgs)
+   }
+
+printf <- function(...) print(noquote(sprintf(...)))
+code.pkgs <- c(
+    'GenomicScores',
+    'phastCons7way.UCSC.hg38',
+    # 'phastCons30way.UCSC.hg38',  loaded from extdata, version/AH problem
+    'phastCons100way.UCSC.hg38')
+
+for(code.pkg in code.pkgs){
+   suppressWarnings(
+      needed <- !require(code.pkg, character.only=TRUE)
+      )
+   printf("%s needed? %s", code.pkg, needed)
+   if(needed)
+      biocGet(code.pkg)
+   } # for
