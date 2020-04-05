f810b0472a949c6b2b9e83291fcd2fb9af6e2503
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-04-05 07:19:03 -0700

    updated path to fimo2/

--- misc/erythropoiesis/marjorieDemos/shiny/tbx15.R
+++ misc/erythropoiesis/marjorieDemos/shiny/tbx15.R
@@ -1,4 +1,4 @@
-directory <- "~/github/TrenaMultiScore/misc/erythropoiesis/threeGenesForMarjorie/fimo2"
+directory <- "~/github/TrenaMultiScore/misc/erythropoiesis/marjorieDemos/fimo2"
 rdata.files <- list.files(path=directory, pattern=".RData")
 
 tbls <- lapply(head(rdata.files, n=-1), function(file){
@@ -13,7 +13,7 @@ dim(tbl.tbx15) # 40571 20
 save(tbl.tbx15, file="tbx15.RData")
 
 # table(subset(tbl.tbx15, gh > 500 & abs(cor) > 0.5 & abs(tss) < 500 & phast30 > 0.9)$targetGene)
-#     BCL6    CPEB4    FOXJ3   MIR144  MIR4732    NATD1     OSR2 PDZK1IP1  RUNDC3A  SLC30A1     SPI1 
-#       31       10        2        4        4        3        6        6       39        3       15 
+#     BCL6    CPEB4    FOXJ3   MIR144  MIR4732    NATD1     OSR2 PDZK1IP1  RUNDC3A  SLC30A1     SPI1
+#       31       10        2        4        4        3        6        6       39        3       15
 
 
